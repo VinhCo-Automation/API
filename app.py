e36775bccadcbe9a740d9ca15b4d20d5b38f2dd7
VinhCo-Automation <covo488@gmail.com>
2025-03-02 06:26:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return "Hello, World!"
+    if request.method == 'POST':
+        data = request.json  # Lấy dữ liệu JSON từ yêu cầu POST
+        return {"received": data}, 200  # Trả về phản hồi
+    return "Hello, World!"  # Phản hồi cho GET
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
