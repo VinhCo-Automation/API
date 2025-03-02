db9b4f825eea7cf3c234908fde8048fba13e83f6
VinhCo-Automation <covo488@gmail.com>
2025-03-02 11:48:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask, request
+import json
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        data = request.json  # Lấy dữ liệu JSON từ request POST
+        data = request.json
         if not data:
-            return {"error": "No JSON data provided"}, 400  # Xử lý lỗi nếu không có dữ liệu
-        print(data)
-        return {"received": data}, 200  # Trả về dữ liệu nhận được
-    return {"message": "Hello, World!"}, 200  # Trả về cho GET hoặc các method khác
+            return {"error": "No JSON data provided"}, 400
+        print(json.dumps(data, indent=4, ensure_ascii=False))  # In đẹp hơn
+        return {"received": data}, 200
+    return {"message": "Hello, World!"}, 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
