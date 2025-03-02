280f03290340cf68b96e8438d7fcc9596c69ab7d
VinhCo-Automation <covo488@gmail.com>
2025-03-02 11:30:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request
-import logging
+#import logging
 
 # Cấu hình logging
-logging.basicConfig(
-    level=logging.INFO,  # Mức log (INFO, DEBUG, ERROR, v.v.)
-    format='%(asctime)s - %(levelname)s - %(message)s'  # Định dạng log
-)
+#logging.basicConfig(
+#    level=logging.INFO,  # Mức log (INFO, DEBUG, ERROR, v.v.)
+#    format='%(asctime)s - %(levelname)s - %(message)s'  # Định dạng log
+#)
 
 app = Flask(__name__)
 
@@ -13,9 +13,10 @@ app = Flask(__name__)
 def home():
     if request.method == 'POST':
         data = request.json  # Lấy dữ liệu JSON từ yêu cầu POST
-        logging.info(f"Dữ liệu nhận được: {data}")  # Ghi log với logging
+#        logging.info(f"Dữ liệu nhận được: {data}")  # Ghi log với logging
+        print(data)
         return {"received": data}, 200
-    return "Hello, World!"
+#    return "Hello, World!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
